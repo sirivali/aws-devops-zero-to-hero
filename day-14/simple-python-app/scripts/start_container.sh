bf571fa522ed1cfbc8cce2bf740e732f043d0fa8
sirivali <149424428+sirivali@users.noreply.github.com>
2025-08-04 13:59:15 +0530

    latest changes - code deploy

--- day-14/simple-python-app/scripts/start_container.sh
+++ day-14/simple-python-app/scripts/start_container.sh
@@ -5,4 +5,5 @@ set -e
 docker pull anuradhya/simple-python-flask-app
 
 # Run the Docker image as a container
-docker run -d -p 5000:5000 anuradhya/simple-python-flask-app
+docker run -d -p 5000:5000 anuradhya/simple-python-flask-app 
+
